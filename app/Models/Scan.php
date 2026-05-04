bbd89753653565c20e6539e2b7120eb6377e1fb7
Abed El-Fattah Amouneh <amounehabed@gmail.com>
2026-05-04 15:49:19 +0300

    model: update Scan.php

--- app/Models/Scan.php
+++ app/Models/Scan.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Scan extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['user_id','game_id','success','radar_level','cost'];
 
     public function user() { return $this->belongsTo(User::class); }
